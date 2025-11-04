7c84ddcc5687d6820e2c1fb8199fece761da9060
Kabuddy768 <benmwangi968@gmail.com>
2025-11-04 20:08:58 +0300

    Change in url in apits

--- thinktrek-business-site/src/services/api.ts
+++ thinktrek-business-site/src/services/api.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
 
 // Use environment variable instead of hardcoded URL
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8088';
+
+
+
+
+
+
+
+
+
+
+
+
+// export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8088';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://thinktrek.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
