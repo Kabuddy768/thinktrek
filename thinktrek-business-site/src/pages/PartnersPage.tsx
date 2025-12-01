1e1483eeef5a3bf1b589677cae7c43cb8c51a0a0
Kabuddy768 <benmwangi968@gmail.com>
2025-12-01 11:21:47 +0300

    Updated patners by removing no set up fees

--- thinktrek-business-site/src/pages/PartnersPage.tsx
+++ thinktrek-business-site/src/pages/PartnersPage.tsx
@@ -317,10 +317,6 @@ export default function PartnersPage() {
           <div className="flex flex-wrap justify-center gap-6 mt-10 text-sm">
             <div className="flex items-center gap-2">
               <CheckCircle className="w-5 h-5" />
-              <span>No Setup Fees</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <CheckCircle className="w-5 h-5" />
               <span>24/7 Support</span>
             </div>
             <div className="flex items-center gap-2">
